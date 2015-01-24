67a7a69286a9b5f542f9da01695aeac8bec1dc1e
Jared Gray <jd.gray@yahoo.com>
2015-01-24 14:50:04 -0800

    test for vacation index and added index view

--- spec/controllers/vacations_controller_spec.rb
+++ spec/controllers/vacations_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe VacationsController, :type => :controller do
-
+	it "should get page for index" do 
+		get :index
+		expect(response).to be_success
+		expect(response).to have_http_status(200)
+	end
 end
