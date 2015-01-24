0f8212e7fd8aee4e718e52a8e9f1259fe06df4fd
Christopher Lynch <YOUR-EMAIL-ADDRESS>
2015-01-24 13:47:04 -0800

    added circle ci gem

--- spec/models/vacation_spec.rb
+++ spec/models/vacation_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Vacation, :type => :model do
 	it "has a valid factory" do
 		expect(FactoryGirl.build(:vacation)).to be_valid 
 	end
+	
 	it "is invalid without a total cost" do
 		cost = FactoryGirl.build(:vacation, total_cost: nil)
 		expect(cost).to be_invalid 
@@ -13,6 +14,7 @@ RSpec.describe Vacation, :type => :model do
 		description = FactoryGirl.build(:vacation, description: nil)
 		expect(description).to be_invalid
 	end
+
 	it "has a total people amount" do
 		people = FactoryGirl.build(:vacation, total_people: nil)
 		expect(people).to be_invalid
