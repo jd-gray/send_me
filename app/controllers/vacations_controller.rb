dace76f1e1d6a0f99fa85d6b984cbc8989c6e769
Christopher Lynch <YOUR-EMAIL-ADDRESS>
2015-01-24 18:29:16 -0800

    added to vacations controller and installed simple form

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -1,15 +1,26 @@
 class VacationsController < ApplicationController
 
+	before_action :vacation_find, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@vacations = Vacation.all 
 	end
 
 	def show
+
 	end
 
 	def new
+		@vacation = Vacation.new
 	end
 
 	def create
+		@vacation = Vacation.new
+		if @vacation.save
+			redirect_to @vacation 
+		else 
+			render :new
+		end
 	end
 
 	def edit
@@ -19,5 +30,10 @@ class VacationsController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+	def vacation_find
+		@vacation = Vacation.find(params[:id])	
 	end
 end
