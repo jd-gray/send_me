4f348321d208d8ea8ea7802d0dbea1736df54ffd
Jared Gray <jd.gray@yahoo.com>
2015-01-24 18:11:06 -0800

    added new test for vacation controller

--- spec/controllers/vacations_controller_spec.rb
+++ spec/controllers/vacations_controller_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe VacationsController, :type => :controller do
+
 	it "should get page for index" do 
 		get :index
 		expect(response).to be_success
 		expect(response).to have_http_status(200)
+	end
+
+	it "should get new" do 
+		get :new
+		assert_response :success
 	end
 end
