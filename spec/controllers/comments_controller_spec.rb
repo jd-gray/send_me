d45deb27871d1c07e486a2b692fc8623a91987f5
Jared Gray <jd.gray@yahoo.com>
2015-01-27 15:49:40 -0800

    removed system pend tests, todo:comment tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, :type => :controller do
 
-  describe "GET body" do
-    it "returns http success" do
-      get :body
-      expect(response).to have_http_status(:success)
-    end
-  end
+	# TODO: Write new test
+  # describe "GET body" do
+  #   it "returns http success" do
+  #     get :body
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
