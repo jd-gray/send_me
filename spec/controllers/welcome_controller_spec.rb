e15ada9a530fea86d678ac6b02287f8cfc237b83
Jared Gray <jd.gray@yahoo.com>
2015-01-26 16:59:42 -0800

    minimal styling to nav/welcome, added welcome test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
 
+	it "should get page for index" do 
+		get :index
+		expect(response).to be_success
+		expect(response).to have_http_status(200)
+	end
+
 end
