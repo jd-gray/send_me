1487ede8a36f0dc611b78051ee6ba8631f956a7d
dromero08 <romerodaniel.k90@gmail.com>
2015-02-01 22:19:23 -0800

    deleted unneccessary static_pages files

--- app/helpers/vacations_helper.rb
+++ app/helpers/vacations_helper.rb
@@ -1,6 +1,7 @@
 module VacationsHelper
   def get_vacation(description)
     description = vacation.description "", "+"
-    @vacation = HTTParty.get("https://sendmeapp.herokuapp.com/api/v1/users/vacations")
+    @vacation = HTTParty.get("http://654eb333.ngrok.com/api/v1/users/vacations?q=#{description}")
+    @picture = @vacation['data']['image_url']
   end
 end
