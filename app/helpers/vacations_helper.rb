06e82e923e5081e36c43dc5a5a25a05e7d53e95b
dromero08 <romerodaniel.k90@gmail.com>
2015-02-01 21:56:49 -0800

    changed the cookies back to unsecure status

--- app/helpers/vacations_helper.rb
+++ app/helpers/vacations_helper.rb
@@ -1,2 +1,6 @@
 module VacationsHelper
+  def get_vacation(description)
+    description = vacation.description "", "+"
+    @vacation = HTTParty.get("https://sendmeapp.herokuapp.com/api/v1/users/vacations")
+  end
 end
