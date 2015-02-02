10b796a0147b6f7432586f11382732bad9c15791
Jared Gray <jd.gray@yahoo.com>
2015-02-01 21:58:57 -0800

    tighter security settings for users implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :user_find, only: [:show, :edit, :update, :destroy]
+	before_action :same_user?, only: [:show, :edit, :update]
 
 	def index
 		@users = User.all
